e5842442eaa0f24ed8fb3cda9c9fd58723b8ffbe
silenkovkonstantin <silenkov@inbox.ru>
2023-02-26 09:50:00 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace Module_9_3_2
+{
+    class Program
+    {
+        public delegate int SubstractDelegate(int a, int b);
+
+        static void Main(string[] args)
+        {
+            SubstractDelegate substractDelegate = Substract;
+            int result = substractDelegate(10, 5);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+
+        static int Substract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
